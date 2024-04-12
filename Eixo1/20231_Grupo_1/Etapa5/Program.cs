70f97c0c8b5835e2eaec2cbb3a307b1b1751a485
gomesluiz <gomes.luiz@gmail.com>
2024-04-12 16:15:47 -0300

    Inclusão de projetos do eixo 1 de 2023_1

---
+++ Eixo1/20231_Grupo_1/Etapa5/Program.cs
@@ -0,0 +1,469 @@
+﻿using System;
+
+using System.Threading;
+
+ 
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        Console.WriteLine("Iniciando Simulador!");
+
+        Thread.Sleep(2000);
+
+ 
+
+        // Inicializando Computador A
+
+        var computadorA = new Computador("Computador A", "aa:aa:aa:aa:aa", "192.168.0.100");
+
+ 
+
+        // Inicializando Computador B
+
+        var computadorB = new Computador("Computador B", "bb:bb:bb:bb:bb", "192.168.0.200");
+
+ 
+
+        // Inicializando Computador C
+
+        var computadorC = new Computador("Computador C", "cc:cc:cc:cc:cc", "192.168.0.300");
+
+ 
+
+        while (true)
+
+        {
+
+            Console.WriteLine("Selecione uma opção:");
+
+            Console.WriteLine("1. Enviar mensagem do Computador A");
+
+            Console.WriteLine("2. Enviar mensagem do Computador B");
+
+            Console.WriteLine("3. Enviar mensagem do Computador C");
+
+            Console.WriteLine("0. Sair");
+
+            Console.Write("Opção selecionada: ");
+
+ 
+
+            int opcao;
+
+            if (int.TryParse(Console.ReadLine(), out opcao))
+
+            {
+
+                Console.WriteLine();
+
+ 
+
+                switch (opcao)
+
+                {
+
+                    case 0:
+
+                        Console.WriteLine("Encerrando o programa...");
+
+                        return;
+
+ 
+
+                    case 1:
+
+                        Console.WriteLine("Computador A");
+
+                        Thread.Sleep(2000);
+
+                        Console.Write("Entre com a Mensagem: ");
+
+                        string mensagemA = Console.ReadLine();
+
+                        Console.WriteLine();
+
+                        Console.WriteLine("Selecione o Meio de Transmissão:");
+
+                        Console.WriteLine("1. Meio de Transmissão Cabo Ethernet");
+
+                        Console.WriteLine("2. Meio de Transmissão Wi-Fi");
+
+                        Console.Write("Opção selecionada: ");
+
+                        int meioA = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine();
+
+ 
+
+                        if (meioA == 1)
+
+                        {
+
+                            Console.WriteLine("Computador A (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.Write("Aplicação -> ");
+
+                            computadorA.EnviarMensagem(mensagemA, computadorB, MeioTransmissao.CaboEthernet);
+
+                            Console.WriteLine();
+
+                            Console.WriteLine("Computador B (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.WriteLine("Mensagem Recebida: " + computadorB.MensagemRecebida);
+
+                        }
+
+                        else if (meioA == 2)
+
+                        {
+
+                            Console.WriteLine("Computador A (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.Write("Aplicação -> ");
+
+                            computadorA.EnviarMensagem(mensagemA, computadorB, MeioTransmissao.WiFi);
+
+                            Console.WriteLine();
+
+                            Console.WriteLine("Computador B (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.WriteLine("Mensagem Recebida: " + computadorB.MensagemRecebida);
+
+                        }
+
+                        else
+
+                        {
+
+                            Console.WriteLine("Opção inválida. Por favor, selecione uma opção válida.");
+
+                        }
+
+ 
+
+                        Console.WriteLine();
+
+                        break;
+
+ 
+
+                    case 2:
+
+                        Console.WriteLine("Computador B");
+
+                        Thread.Sleep(2000);
+
+                        Console.Write("Entre com a Mensagem: ");
+
+                        string mensagemB = Console.ReadLine();
+
+                        Console.WriteLine();
+
+                        Console.WriteLine("Selecione o Meio de Transmissão:");
+
+                        Console.WriteLine("1. Meio de Transmissão Cabo Ethernet");
+
+                        Console.WriteLine("2. Meio de Transmissão Wi-Fi");
+
+                        Console.Write("Opção selecionada: ");
+
+                        int meioB = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine();
+
+ 
+
+                        if (meioB == 1)
+
+                        {
+
+                            Console.WriteLine("Computador B (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.Write("Aplicação -> ");
+
+                            computadorB.EnviarMensagem(mensagemB, computadorA, MeioTransmissao.CaboEthernet);
+
+                            Console.WriteLine();
+
+                            Console.WriteLine("Computador A (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.WriteLine("Mensagem Recebida: " + computadorA.MensagemRecebida);
+
+                        }
+
+                        else if (meioB == 2)
+
+                        {
+
+                            Console.WriteLine("Computador B (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.Write("Aplicação -> ");
+
+                            computadorB.EnviarMensagem(mensagemB, computadorA, MeioTransmissao.WiFi);
+
+                            Console.WriteLine();
+
+                            Console.WriteLine("Computador A (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.WriteLine("Mensagem Recebida: " + computadorA.MensagemRecebida);
+
+                        }
+
+                        else
+
+                        {
+
+                            Console.WriteLine("Opção inválida. Por favor, selecione uma opção válida.");
+
+                        }
+
+ 
+
+                        Console.WriteLine();
+
+                        break;
+
+ 
+
+                    case 3:
+
+                        Console.WriteLine("Computador C");
+
+                        Thread.Sleep(2000);
+
+                        Console.Write("Entre com a Mensagem: ");
+
+                        string mensagemC = Console.ReadLine();
+
+                        Console.WriteLine();
+
+                        Console.WriteLine("Selecione o Meio de Transmissão:");
+
+                        Console.WriteLine("1. Meio de Transmissão Cabo Ethernet");
+
+                        Console.WriteLine("2. Meio de Transmissão Wi-Fi");
+
+                        Console.Write("Opção selecionada: ");
+
+                        int meioC = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine();
+
+ 
+
+                        if (meioC == 1)
+
+                        {
+
+                            Console.WriteLine("Computador C (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.Write("Aplicação -> ");
+
+                            computadorC.EnviarMensagem(mensagemC, computadorA, MeioTransmissao.CaboEthernet);
+
+                            Console.WriteLine();
+
+                            Console.WriteLine("Computador A (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.WriteLine("Mensagem Recebida: " + computadorA.MensagemRecebida);
+
+                        }
+
+                        else if (meioC == 2)
+
+                        {
+
+                            Console.WriteLine("Computador C (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.Write("Aplicação -> ");
+
+                            computadorC.EnviarMensagem(mensagemC, computadorA, MeioTransmissao.WiFi);
+
+                            Console.WriteLine();
+
+                            Console.WriteLine("Computador A (Camada de Aplicação)");
+
+                            Thread.Sleep(2000);
+
+                            Console.WriteLine("Mensagem Recebida: " + computadorA.MensagemRecebida);
+
+                        }
+
+                        else
+
+                        {
+
+                            Console.WriteLine("Opção inválida. Por favor, selecione uma opção válida.");
+
+                        }
+
+ 
+
+                        Console.WriteLine();
+
+                        break;
+
+ 
+
+                    default:
+
+                        Console.WriteLine("Opção inválida. Por favor, selecione uma opção válida.");
+
+                        Console.WriteLine();
+
+                        break;
+
+                }
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Opção inválida. Por favor, selecione uma opção válida.");
+
+                Console.WriteLine();
+
+            }
+
+        }
+
+    }
+
+}
+
+ 
+
+class Computador
+
+{
+
+    public string Nome { get; }
+
+    public string EndMAC { get; }
+
+    public string EndIP { get; }
+
+    public string MensagemRecebida { get; private set; }
+
+ 
+
+    public Computador(string nome, string endMAC, string endIP)
+
+    {
+
+        Nome = nome;
+
+        EndMAC = endMAC;
+
+        EndIP = endIP;
+
+    }
+
+ 
+
+    public void EnviarMensagem(string mensagem, Computador destino, MeioTransmissao meioTransmissao)
+
+    {
+
+        Console.WriteLine("Camada de Aplicação -> Camada de Transporte -> Camada de Rede -> Camada de Enlace");
+
+        Thread.Sleep(2000);
+
+ 
+
+        Console.WriteLine("Enviando mensagem de {0} para {1}: {2}", Nome, destino.Nome, mensagem);
+
+        Thread.Sleep(2000);
+
+ 
+
+        // Simulando os meios físicos
+
+        if (meioTransmissao == MeioTransmissao.CaboEthernet)
+
+        {
+
+            Console.WriteLine("Transmissão através de Cabo Ethernet");
+
+            Thread.Sleep(2000);
+
+        }
+
+        else if (meioTransmissao == MeioTransmissao.WiFi)
+
+        {
+
+            Console.WriteLine("Transmissão através de Wi-Fi");
+
+            Thread.Sleep(2000);
+
+        }
+
+ 
+
+        Console.WriteLine("Destino -> Camada de Enlace -> Camada de Rede -> Camada de Transporte -> Camada de Aplicação");
+
+        destino.ReceberMensagem(mensagem);
+
+    }
+
+ 
+
+    private void ReceberMensagem(string mensagem)
+
+    {
+
+        Console.WriteLine("Recebendo mensagem em {0}: {1}", Nome, mensagem);
+
+        Thread.Sleep(2000);
+
+ 
+
+        MensagemRecebida = mensagem;
+
+    }
+
+}
+
+ 
+
+enum MeioTransmissao
+
+{
+
+    CaboEthernet,
+
+    WiFi
+
+}
---
+++ Eixo1/20231_Grupo_2/Apresentação do Projeto/Program.cs
@@ -0,0 +1,248 @@
+﻿﻿using System;
+using System.Threading;
+
+
+namespace Classes
+{
+
+    static class MeioFisico
+    {
+        public static Computador Comp1 = new Computador();
+        public static Computador Comp2 = new Computador();
+        public static Computador Comp3 = new Computador();
+        public static Computador Comp4 = new Computador();
+    
+        public static void Send(Computador.Enlace pdu_enlace) {
+            Thread.Sleep(1000);
+            Console.WriteLine("Meio Fisico");
+            
+            if (pdu_enlace.MACdest == Comp1.EndMAC) {
+                Console.WriteLine("Computador " + Comp1.nome);
+                Comp1.enlace.Receive(pdu_enlace);
+            }
+            else if (pdu_enlace.MACdest == Comp2.EndMAC) {
+                Console.WriteLine("Computador " + Comp2.nome);
+                Comp2.enlace.Receive(pdu_enlace);
+            }
+            else if (pdu_enlace.MACdest == Comp3.EndMAC) {
+                Console.WriteLine("Computador " + Comp3.nome);
+                Comp3.enlace.Receive(pdu_enlace);
+            }
+            else if (pdu_enlace.MACdest == Comp4.EndMAC) {
+                Console.WriteLine("Computador " + Comp4.nome);
+                Comp4.enlace.Receive(pdu_enlace);
+            }
+        }
+
+        public static string GetMacAddress(string IPDest) {
+            if (IPDest == "192.168.0.100")
+              return "aa:aa:aa:aa:aa:aa";
+            if (IPDest == "192.168.0.200")
+              return "bb:bb:bb:bb:bb:bb";
+            if (IPDest == "192.168.0.300")
+              return "cc:cc:cc:cc:cc:cc";
+            if (IPDest == "192.168.0.400")
+              return "dd:dd:dd:dd:dd:dd";  
+            else return "00:00:00:00:00:00";  
+        }
+
+        public static string GetIPAddress(string Name) {
+            if (Name == "A")
+              return "192.168.0.100";
+            if (Name == "B")
+              return "192.168.0.200";
+            if (Name == "C")
+              return "192.168.0.300";
+            if (Name == "D")
+              return "192.168.0.400";
+            else return "000.000.000.000";  
+        }
+    }
+
+    class Computador 
+    {
+        public string nome = "sem nome";
+        public string EndMAC = "00:00:00:00:00:00";
+        public string EndIP = "000.000.000";
+        public string IPDest = "000.000.000";
+  
+        public Aplicacao aplicacao = new Aplicacao();
+        public Transporte transporte = new Transporte();
+        public Rede rede = new Rede();
+        public Enlace enlace = new Enlace();
+
+        public void Executa() {
+            
+            Console.WriteLine("De Computador " + EndIP + " para Computador " + IPDest);
+
+            Console.WriteLine("Entre com  a mensagem: ");
+            Aplicacao aplicacao = new Aplicacao();
+            aplicacao.dado = Console.ReadLine() + "";
+            Thread.Sleep(1000);
+            Console.WriteLine("Computador " + nome);
+            aplicacao.Send(IPDest);
+        }
+        public class Enlace
+        {   
+            // cabeçalho
+            public string MACdest = "";
+            public string MACorig = "";
+            // dado
+            public Rede dado = new Rede();
+            public void Send()
+            {
+               
+                this.MACdest = MeioFisico.GetMacAddress(this.dado.IPdest);
+                Thread.Sleep(1000);
+                Console.Write("Send-Enlace -> ");
+                MeioFisico.Send(this);
+            }
+            public void Receive(Enlace pdu_enlace) 
+            {               
+                Thread.Sleep(1000);            
+                Console.Write("Receive-Enlace -> ");
+                var servico_rede = new Rede();
+                servico_rede.Receive(pdu_enlace.dado);	            
+            }                
+        }
+        public class Rede
+        {
+            public string ProtocoloIP = "";
+            public string IPdest = "";
+            public string IPorig = "";
+            public Transporte dado = new Transporte();
+            public void Send() 
+            {            
+                Thread.Sleep(1000);
+                this.ProtocoloIP = "IPV4";
+                Console.Write("Send-Rede -> ");
+                var pdu_enlace = new Enlace();
+                pdu_enlace.dado = this;
+                pdu_enlace.Send();        
+            }            
+            public void Receive(Rede pdu_rede) 
+            {               
+                Thread.Sleep(1000);
+                Console.Write("Receive-Rede -> ");
+                var servico_transporte = new Transporte();
+                servico_transporte.Receive(pdu_rede.dado);	            		        
+            } 
+        }
+        public class Transporte
+        {
+            public static readonly int TCP = 1;
+            public static readonly int UDP = 2;
+            public int Protocolo;
+            public int porta_origem;
+            public int porta_destino;
+            public string dado="";
+            
+            public void Send(string IPdest) 
+            {
+                Thread.Sleep(1000);
+                this.Protocolo = Transporte.UDP;
+                this.porta_origem = 8080;
+                this.porta_destino = 100;  // porta de serviço 
+                Console.Write("Send-Transporte -> ");
+                var pdu_rede = new Rede();
+                pdu_rede.IPdest = IPdest;
+                pdu_rede.dado = this;
+                pdu_rede.Send();
+            }
+            public void Receive(Transporte pdu_transporte) 
+            {     
+                Thread.Sleep(1000);
+                Console.Write("Receive-Transporte -> ");
+                var servico_aplicacao = new Aplicacao();
+                servico_aplicacao.Receive(pdu_transporte.dado);          
+            }
+        }
+        public class Aplicacao
+        {
+            public string dado="";       
+            public void Send(string IPDest) 
+            {
+                Thread.Sleep(1000);
+                Console.Write(dado + " -> " + "Send-Aplicação -> ");
+                var transporte = new Transporte();
+                transporte.dado = dado;
+                transporte.Send(IPDest);	
+                
+            }
+            public void Receive(string dado) 
+            {     
+                Console.Write("Receive-Aplicação -> ");
+                Console.WriteLine(dado);
+            }
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Inicializando Computador 1
+            var computador1 = new Computador();
+            computador1.nome = "A";
+            computador1.EndMAC = "aa:aa:aa:aa:aa:aa";
+            computador1.EndIP = "192.168.0.100";
+
+            // Inicializando Computador 2
+            var computador2 = new Computador();
+            computador2.nome = "B";
+            computador2.EndMAC = "bb:bb:bb:bb:bb:bb";
+            computador2.EndIP = "192.168.0.200";
+
+            // Inicializando Computador 3
+            var computador3 = new Computador();
+            computador3.nome = "C";
+            computador3.EndMAC = "cc:cc:cc:cc:cc:cc";
+            computador3.EndIP = "192.168.0.300";
+
+            // Inicializando Computador 4
+            var computador4 = new Computador();
+            computador4.nome = "D";
+            computador4.EndMAC = "dd:dd:dd:dd:dd:dd";
+            computador4.EndIP = "192.168.0.400";
+
+            // Estabelecendo conexão física entre as máquinas
+            MeioFisico.Comp1 = computador1;
+            MeioFisico.Comp2 = computador2;
+            MeioFisico.Comp3 = computador3;
+            MeioFisico.Comp4 = computador4;
+
+
+            // Inicializando Simulação
+            Console.WriteLine("Iniciando Simulador!!");
+            Thread.Sleep(1000);
+
+            string Origem = "A";
+            string Destino = "C";
+ 
+            Console.WriteLine("Computador "+ Origem + " para Computador " + Destino);
+            
+            if (Origem == "A") {
+              computador1.IPDest = MeioFisico.GetIPAddress(Destino);  
+              computador1.Executa();
+            }  
+            else if (Origem == "B") {
+              computador2.IPDest = MeioFisico.GetIPAddress(Destino);  
+              computador2.Executa();
+            }  
+            else if (Origem == "C") {
+              computador3.IPDest = MeioFisico.GetIPAddress(Destino);  
+              computador3.Executa();
+            }  
+            else if (Origem == "D") {
+              computador4.IPDest = MeioFisico.GetIPAddress(Destino);  
+              computador4.Executa();
+            }  
+
+            Console.WriteLine("Transmissão do dado bem-sucedida. Aperte enter para sair.");
+            Console.ReadLine();
+
+        }    
+    }    
+}
